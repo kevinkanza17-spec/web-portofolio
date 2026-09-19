d7f87f2ecdc2a5f39388bdd61028ade421b7573d
kevinkanza17-spec <kevinkanza17@gmail.com>
2026-09-19 15:34:58 +0700

    navbar nya sek error

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// Mengambil elemen dari HTML
-const menuButton = document.getElementById("menuButton");
+// Ambil semua link navbar
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+// Tambahkan event ketika link diklik
+navLinks.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        // Hapus class active dari semua link
+        navLinks.forEach(function(item) {
+            item.classList.remove("active");
+        });
+
+        // Tambahkan active ke link yang diklik
+        this.classList.add("active");
+
+    });
+
+// Hamburger Menu
+
+const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
 
-// Ketika tombol menu diklik
-menuButton.addEventListener("click", function () {
-
-    // Menampilkan / menyembunyikan menu
-    navMenu.classList.toggle("show");
-
+menuToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
 });
